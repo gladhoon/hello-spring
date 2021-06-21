8e738281ff9af4eb0084ec24426fb070c905c85c
gladhoon <76650410+gladhoon@users.noreply.github.com>
2021-06-21 12:02:40 +0900

    Add files via upload

---
+++ hello-spring/src/test/java/hello/hellospring/repository/MemoryMemberRepositoryTest.java
@@ -0,0 +1,67 @@
+package hello.hellospring.repository;
+
+import hello.hellospring.domain.Member;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class MemoryMemberRepositoryTest {
+
+    MemoryMemberRepository repository = new MemoryMemberRepository();
+
+//    테스트가 끝날 때 마다 레포지토리가 깔끔하게 지워지는 코드 (매 Test가 끝날 때 마다 실행된다.)
+    @AfterEach
+    public void afterEach() {
+        repository.clearStore();
+    }
+
+    @Test
+    public void save() {
+        Member member = new Member();
+        member.setName("spring");
+
+        repository.save(member);
+
+        Member result = repository.findById(member.getId()).get(); // Optional에서는 get으로 값을 꺼낼 수 있다.
+        assertThat(member).isEqualTo(result);
+//        Assertions.assertEquals(member, result);  기대하는 값이 앞으로 (member, result)
+//        System.out.println("result = " + (result == member)); 좋지 않은 방법 (출력 사용)
+    }
+
+    @Test
+    public void findByName() {
+        Member member1 = new Member();
+        member1.setName("spring1");
+        repository.save(member1);
+
+        Member member2 = new Member();
+        member2.setName("spring1");
+        repository.save(member2);
+
+        Member result = repository.findByName("spring1").get();
+//        Member result = repository.findByName("spring2").get();
+        assertThat(result).isEqualTo(member1);
+    }
+
+    @Test
+    public void findAll() {
+        Member member1 = new Member();
+        member1.setName("spring1");
+        repository.save(member1);
+
+        Member member2 = new Member();
+        member2.setName("spring1");
+        repository.save(member2);
+
+        List<Member> result = repository.findAll();
+
+        assertThat(result.size()).isEqualTo(2);
+    }
+
+}
+
+
