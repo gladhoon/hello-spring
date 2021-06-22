2921afeb42fe2d8ef2c653abc6afd2f159d06ba9
gladhoon <76650410+gladhoon@users.noreply.github.com>
2021-06-22 17:54:18 +0900

    Add files via upload

--- hello-spring/src/main/java/hello/hellospring/service/MemberService.java
+++ hello-spring/src/main/java/hello/hellospring/service/MemberService.java
@@ -3,14 +3,17 @@ package hello.hellospring.service;
 import hello.hellospring.domain.Member;
 import hello.hellospring.repository.MemberRepository;
 import hello.hellospring.repository.MemoryMemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
 public class MemberService {
-    
+
     private final MemberRepository memberRepository;
     // constructor을 이용해서 만듦, Test Case에서 Repository랑 다른 Repository를 사용하기 때문이다.
+
     public MemberService(MemberRepository memberRepository) {
         this.memberRepository = memberRepository;
     }
